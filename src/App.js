d938bfa7b3408e997f27410d82e15b01903012cb
AyushSinghTeotia <ayushteotia@questglt.org>
2022-12-29 10:46:03 +0530

    add alerts and update handle functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 function App() {
  const [mode, setmode] = useState('light');
  const [modename, setmodename] = useState("Enable dark mode");
+ const [alert,setAlert]=useState(null);
+
+ const showalert=(massage,type)=>{
+  setAlert({
+    msg:massage,
+    type:type
+  })
+ }
 
  const togggleMode=()=>{
        if(mode==="light"){
         setmode('dark');
         setmodename("Disable dark mode");
         document.body.style.backgroundColor="grey";
+        showalert("dark mode has been enabled","success");
        }
        
        else{
         setmode("light")
         setmodename("Enable dark mode");
         document.body.style.backgroundColor="white";
+        showalert("light mode has been enabled","success");
        }
  }
 
@@ -25,12 +36,14 @@ function App() {
     <>
     
     <Navbar title="TextUtils2" mode={mode} toggleMode={togggleMode} modename={modename} AboutText="About Us"/>
+    <Alert alert={alert}/>
     <div className="container my-3">
-    <TextForm heading="Enter a Text" mode={mode}/>
+    <TextForm heading="Enter a Text" mode={mode} showalert={showalert}/>
     </div>
     {/* <div className="container my-10">
     <About/>
     </div> */}
+   
     
   
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,30 +7,82 @@ export default function TextForm(props) {
     console.log("upper case was click"+text);
     let newtext=text.toUpperCase();
     setText(newtext);
+    if(newtext.length>0){
+      props.showalert("Converted to upper case","success");
+    }
+    else{
+      props.showalert("Please enter text","warning");
+    }
   }
   const handleLpClick=()=>{
     console.log("lower case was click"+text);
     let newtext=text.toLowerCase();
     setText(newtext);
+    if(newtext.length>0){
+      props.showalert("Converted to lower case","success");
+    }
+    else{
+      props.showalert("Please enter text","warning");
+    }
   }
   const handleOnChange=(event)=>{
     setText(event.target.value);
   }
   const handleASCIIClick=()=>{
 
-    for(let i = 0; i < text.length; i++){
-    let newtext=text.charCodeAt(i);
-    var newtext1= newtext.toString();
-   }
-   setText(newtext1);
+    let newtext=text;
+    if(newtext.length===1)
+    {
+          var newtext1 =newtext.charCodeAt(0);
+          var newtext2=newtext1.toString();
+          setText(newtext2);
+          props.showalert("Converted to ascii","success");
+
+    }
+    else if(newtext.length>1){
+      props.showalert("You can convert only charater to ascii","warning");
+
+
+    }
+    else{
+      props.showalert("Please enter text","warning");
+    }
   }
+
+
+
+  //   for(let i = 0; i < text.length; i++){
+  //   //var newtext=text.charCodeAt(i);
+  //   var newtext1= newtext.toString();
+  //  }
+  //  setText(newtext1);
+  //  console.log(setText)
+  //  if(newtext.length>0){
+  //   props.showalert("Converted to ascii","success");
+  // }
+  
+  
   const handleClerText=()=>{
-    let newtext="";
-    setText(newtext);
-  }
+    let newtext=text;
+     if(newtext.length>0){
+      setText("");
+      props.showalert("Text has been clear","success");
+    }
+    else{
+      props.showalert("Please enter text","warning");
+    }
+    }
+  
   const handleExtraspce=()=>{
-    let newText = text.split(/[ ]+/);
-    setText (newText.join(" "));
+    let text1=text;
+    if(text1.length>0){
+      let newText = text.split(/[ ]+/);
+      setText (newText.join(" "));
+      props.showalert("Extra space has been removed","success");
+    }
+    else{
+      props.showalert("Please enter text","warning");
+    }
   }
   // const handleEmail=()=>{
   //   let newtext=text;
@@ -39,7 +91,7 @@ export default function TextForm(props) {
   // }
   return (
     <>
-    <div className="container" style={{color: props.mode==="dark"?"white":"black"}}>
+    <div className="container" style={{color:props.mode === "dark"?"white":"black"}}>
       <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea
@@ -48,7 +100,7 @@ export default function TextForm(props) {
           rows="8"
           value={text}
           onChange={handleOnChange}
-          style={{backgroundColor:props.mode==="dark"?"grey":"white",color:"white"}}
+          style={{backgroundColor:props.mode==="dark"?"grey":"white",color:props.mode==="dark"?"white":"black"}}
         ></textarea>
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To UpperCase</button>
@@ -60,7 +112,7 @@ export default function TextForm(props) {
    
     <div className="container my-3" style={{color: props.mode==="dark"?"white":"black"}} >
     <h1>Your Text Summary </h1>
-    <p>{text.split(" ").length} words and {text.length} characters</p>
+    <p>{text.split(" ").length-1} words and {text.length} characters</p>
     <p>{0.008*text.split(" ").length} Minutes read</p>
     <h2>Preview</h2>
     <p>{text.length>0?text:"please enter somthing for preview"}</p>
